bad9e3383d10df8dbc09f5cd9733fb287a20dac0
Amy Spears <amspears007@gmail.com>
2023-06-10 16:01:51 -0600

    refactor more_than_five? method in controller to have before action

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,5 +1,5 @@
 class CouponsController < ApplicationController
-  # before_action :more_than_five?, only: [:new, :create, :index]
+  before_action :more_than_five?, only: [:create]
 
   def index
     @merchant = Merchant.find( params[:merchant_id])
@@ -18,10 +18,7 @@ class CouponsController < ApplicationController
   def create
     merchant = Merchant.find( params[:merchant_id])
     coupon = merchant.coupons.new(coupon_params)
-    if merchant.active_coupon_count >= 5
-      redirect_to new_merchant_coupon_path(merchant)
-      flash[:alert] = "Can only have 5 active coupons"
-    elsif coupon.save
+    if coupon.save
       redirect_to merchant_coupons_path(merchant)  
     else
       redirect_to new_merchant_coupon_path(merchant)
@@ -34,9 +31,11 @@ class CouponsController < ApplicationController
     params.permit(:name, :unique_code, :amount_off, :discount, :status)
   end
 
-  # def more_than_five?
-  #   if merchant.active_coupon_count >= 5
-  #     redirect_to new_merchant_coupon_path(merchant)
-  #     flash[:alert] = "Can only have 5 active coupons"
-  # end
+  def more_than_five?
+    merchant = Merchant.find( params[:merchant_id])
+    if merchant.active_coupon_count >= 5
+      redirect_to new_merchant_coupon_path(merchant)
+      flash[:alert] = "Can only have 5 active coupons"
+    end
+  end
 end
